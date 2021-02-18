a8aad9537c373d2026febc621ff09fbf70c66932
DaniilOr <facedan123@gmail.com>
2021-02-18 18:45:27 +0600

    add transactions

---
+++ services/transactions/cmd/app/server.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"context"
+	serverPb "github.com/DaniilOr/microtracing/services/transactions/pkg/server"
+	"log"
+	"github.com/DaniilOr/microtracing/services/transactions/pkg/transactions"
+)
+
+type Server struct {
+	transactionsSvc *transactions.Service
+	ctx context.Context
+}
+
+func NewServer(transactionsSvc *transactions.Service, ctx context.Context) *Server {
+	return &Server{transactionsSvc: transactionsSvc, ctx: ctx}
+}
+
+func (s *Server) Transactions(ctx context.Context, request * serverPb.TransactionsRequest) (*serverPb.TransactionsResponse, error){
+
+	userID := request.UserID
+
+	records, err := s.transactionsSvc.Transactions(ctx, userID)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	var response serverPb.TransactionsResponse
+	for _, record := range records {
+		response.Items = append(response.Items, &serverPb.Transaction{
+			Id:       record.ID,
+			UserId:   record.UserID,
+			Category: record.Category,
+			Amount:   record.Amount,
+			Created:  record.Created,
+		})
+	}
+	return &response, nil
+}
--- services/transactions/go.mod
+++ services/transactions/go.mod
@@ -1,3 +1,9 @@
 module github.com/DaniilOr/microtracing/services/transactions
 
 go 1.15
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.10.1 // indirect
+	google.golang.org/grpc v1.35.0 // indirect
+)
